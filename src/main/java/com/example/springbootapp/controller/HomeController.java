8a6ed0cc4d058332e4c17f3d2c8c841378213fd1
vishal <vishal.bairwa94@gmail.com>
2022-05-07 12:55:12 +0530

    first commit

---
+++ src/main/java/com/example/springbootapp/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.springbootapp.controller;
+
+import com.example.springbootapp.service.HelloWorld;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    @Autowired
+    private HelloWorld helloWorld;
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> getHello() {
+        return new ResponseEntity<>(helloWorld.printHello(), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/springbootapp/service/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.springbootapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorld {
+
+    public String printHello() {
+        return "Hello World";
+    }
+}
